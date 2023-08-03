062fb7205f8b98bca0faf3d9ebe2eb2055eeda28
Ishfaqdev <Ishfaqbcs@gmail.com>
2023-08-03 12:13:58 +0500

    Whatsapp button has been Added

--- cars/urls.py
+++ cars/urls.py
@@ -4,7 +4,7 @@ app_name = 'cars'
 urlpatterns = [
     path('', views.home, name='home'),
     path('car_details/<int:car_id>/', views.car_detail, name='car_details'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
-    path('services', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
 ]
