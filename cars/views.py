c4f8d326a34f17e1eb33c7046bd32fa37bf117ad
Ishfaqdev <Ishfaqbcs@gmail.com>
2023-08-03 19:17:43 +0500

    All Cars, Search page has been Added

--- cars/views.py
+++ cars/views.py
@@ -12,12 +12,20 @@ def home(request):
     return render(request, 'cars/home.html', context)
 
 
+def all_cars(request):
+    return render(request, 'cars/cars_list.html')
+
+
 def car_detail(request, car_id):
     car_detail = Car.objects.get(pk=car_id)
     context = {
         'car_detail': car_detail,
     }
     return render(request, 'cars/cars_detail.html', context)
+
+
+def search_cars(request):
+    return render(request, 'cars/search.html')
 
 
 def about(request):
